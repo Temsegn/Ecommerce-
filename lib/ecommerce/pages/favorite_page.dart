34632494ec628eb1714ba37a11618734a4cf9b9a
Temesgen Marie <tommr2323@gmail.com>
2025-03-08 14:53:18 +0300

    the favorite page update automatically

--- lib/ecommerce/pages/favorite_page.dart
+++ lib/ecommerce/pages/favorite_page.dart
@@ -1,34 +1,36 @@
 import 'package:flutter/material.dart';
 import '../model/favorite_model.dart';
 
-class FavoritePage extends StatelessWidget {
+class FavoritePage extends StatefulWidget {
   const FavoritePage({super.key});
-  
+
+  @override
+  _FavoritePageState createState() => _FavoritePageState();
+}
+
+class _FavoritePageState extends State<FavoritePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: Container(
         padding: EdgeInsets.all(16),
-        child: Favorite.favorites.isEmpty 
-            ? Center(child: Text("Your Favorite is empty"))
+        child: Favorite.favorites.isEmpty
+            ? Center(child: Text("Your Favorites are empty"))
             : ListView.builder(
-                itemCount: Favorite.favorites.length,  // Get the count of items in the cart
+                itemCount: Favorite.favorites.length,
                 itemBuilder: (context, index) {
-                  final product = Favorite.favorites[index]; // Get the individual product
+                  final product = Favorite.favorites[index];
                   return ListTile(
-                    leading: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10)
-                      ),
-                      child: Image.network(product.imageUrl)),  // Use the 'imageUrl' of the product
-                    title: Text(product.title),  // Use the 'title' of the product
-                    subtitle: Text('ETB ${product.price.toStringAsFixed(2)}'),  // Display the price
+                    leading: Image.network(product.imageUrl),
+                    title: Text(product.title),
+                    subtitle: Text('ETB ${product.price.toStringAsFixed(2)}'),
                     trailing: IconButton(
-                      icon: Icon(Icons.delete,color: Colors.redAccent,),
+                      icon: Icon(Icons.delete, color: Colors.redAccent),
                       onPressed: () {
-                        // Remove the item from the cart when clicked
-                        Favorite.removeItem(product);
+                        // Update UI with setState
+                        setState(() {
+                          Favorite.removeItem(product);
+                        });
                       },
                     ),
                   );
